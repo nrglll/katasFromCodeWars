992e1e05bdb5dfd2ff70b0b85948ce07ec5398de
Nurgul Aygun <nnurgulklc@gmail.com>
2020-05-08 17:07:58 +0100

    Kata explanations added

--- kata3.py
+++ kata3.py
@@ -2,24 +2,38 @@
 """
 Created on Wed May  6 13:36:47 2020
 
-@author: asus
+@author: Nurgul Aygun
 """
+# =============================================================================
+# Kata explaination:
+# You probably know the "like" system from Facebook and other pages. People can
+# "like" blog posts, pictures or other items. We want to create the text that should 
+# be displayed next to such an item.
+# 
+# Implement a function likes :: [String] -> String, which must take in input array, 
+# containing the names of people who like an item. It must return 
+# the display text as shown in the examples:
+# 
+# likes [] // must be "no one likes this"
+# likes ["Peter"] // must be "Peter likes this"
+# likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+# likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+# likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+# For 4 or more names, the number in and 2 others simply increases.
+# =============================================================================
 
-def validate_pin(pin):
+
+def likes(names):
+    l = len(names)
+    if l == 1: return "{} likes this".format(names[0])
+    elif l == 2: return "{} and {} like this".format(names[0], names[1])
+    elif l == 3: return "{}, {} and {} like this".format(names[0], names[1], names[2])
+    elif l > 3:  return "{}, {} and {} others like this".format(names[0], names[1], (l-2))
+    else: return "no one likes this"
+
+# =============================================================================
+# print(likes(["elif", "ahmet", "mehmet", "veli"]))
+# =============================================================================
 
         
-    if len(pin) == 4 or len(pin) == 6:
-        try:
-            int(pin) == int
-            
-            if int(pin)<0:
-                return False
-            else: 
-                return True
-        except: 
-            return False
         
-    else:
-        return False
-    
-print(validate_pin("+98476"))
---
+++ kata4.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  8 17:05:20 2020
+
+@author: Nurgul Aygun
+"""
+
+
